99a28b27913032b832d11284b581983abf56d9c3
Adam Winnifrith <113369502+Germanki@users.noreply.github.com>
2024-03-18 16:47:53 +0000

    Create 240318_mutscan_for_Lily.R

---
+++ tallynn/R/240318_mutscan_for_Lily.R
@@ -0,0 +1,7 @@
+output <- digestFastqs(
+  fastqForward = "/home/ubuntu/AWTallyNN/tallynn/python/240318_IDTGblock_Flipped_2.fastq",
+  elementsForward = "SPVS",
+  primerForward = "GCTGGTGAGGTTGCGGATAACG",
+  elementLengthsForward = c(-1, 22, 18, -1),
+  # Add other arguments as needed
+)
